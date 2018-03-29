1f85f323cab88069db8b8f71e67b4aebdc6da3c9
Mike <aeonlucid@outlook.com>
2018-03-29 05:15:46 +0200

    Add project files.

---
+++ MusicallyTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MusicallyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
